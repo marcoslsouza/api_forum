f7d6860dcbb4a8737012fc15285d42e38d29b0d1
marcoslsouza <marcoslsouzadf@gmail.com>
2021-10-13 01:06:44 -0300

    first commit

---
+++ src/main/java/com/github/marcoslsouza/api_forum/ApiForumApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcoslsouza.api_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/marcoslsouza/api_forum/ApiForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcoslsouza.api_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
