bdc42d6c15badf787316f3e757e355869c523413
marcoslsouza <marcoslsouzadf@gmail.com>
2021-10-16 10:49:38 -0300

    Criacao do DTO para topicos

--- src/main/java/com/github/marcoslsouza/api_forum/controller/TopicosController.java
+++ src/main/java/com/github/marcoslsouza/api_forum/controller/TopicosController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.marcoslsouza.api_forum.dto.TopicoDto;
 import com.github.marcoslsouza.api_forum.modelo.Curso;
 import com.github.marcoslsouza.api_forum.modelo.Topico;
 
@@ -13,8 +14,8 @@ import com.github.marcoslsouza.api_forum.modelo.Topico;
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	public List<Topico> lista() {
+	public List<TopicoDto> lista() {
 		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 	}
 }
