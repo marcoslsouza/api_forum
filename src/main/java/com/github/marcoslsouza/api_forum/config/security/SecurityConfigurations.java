6b8be7f69dec00fd837896f100bb4422f321a806
marcoslsouza <marcoslsouzadf@gmail.com>
2021-12-28 11:39:39 -0300

    Liberando o acesso a lista de topicos e detalhes

--- src/main/java/com/github/marcoslsouza/api_forum/config/security/SecurityConfigurations.java
+++ src/main/java/com/github/marcoslsouza/api_forum/config/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package com.github.marcoslsouza.api_forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,5 +14,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+	// Configuracoes de autenticacao
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+	}
 	
+	// Configuracoes de autorizacao
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+	}
+	
+	// Configuracoes de recursos estaticos (js, css, imagens e etc)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+	}
 }
